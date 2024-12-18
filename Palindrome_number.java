6d104a5a7f2b67bd8161b0503c37262f913ec468
Sanjaiweb <sanjaisanjai7182@gmail.com>
2024-12-18 19:20:52 +0530

    Palindrome number

---
+++ Palindrome_number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+       String num = x+"";
+        String rev ="";
+       for(int i=num.length()-1;i>=0;i--){
+           rev +=num.charAt(i);
+         } 
+         if(num.equals(rev)){
+            return true;
+         }else{
+            return false;
+         }
+    }
+}
