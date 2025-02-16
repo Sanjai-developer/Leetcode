f66f39d82b0dd41ceea998109b818ee3745eda74
Sanjai N <136091407+Sanjai-developer@users.noreply.github.com>
2025-02-16 09:31:56 +0530

    Time: 69 ms (28.95%) | Memory: 45.9 MB (23.69%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.java
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.java
@@ -9,12 +9,12 @@ class Solution {
         Map<Character,Integer> freq1 = new HashMap<>();
         Map<Character,Integer> freq2 = new HashMap<>();
 
-        for(char c : word1.toCharArray()){
-            freq1.put(c,freq1.getOrDefault(c,0)+1);
-        }
+      
 
-        for(char c : word2.toCharArray()){
-            freq2.put(c,freq2.getOrDefault(c,0)+1);
+        for(int i=0;i<word1.length();i++){
+
+            freq1.put(word1.charAt(i),freq1.getOrDefault(word1.charAt(i),0)+1);
+            freq2.put(word2.charAt(i),freq2.getOrDefault(word2.charAt(i),0)+1);
         }
         if(!freq1.keySet().equals(freq2.keySet())){
             return false;
