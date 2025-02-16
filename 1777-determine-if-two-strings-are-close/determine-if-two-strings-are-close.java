598cad7547e10c0d8a752882c3e6d89b6265475c
Sanjai N <136091407+Sanjai-developer@users.noreply.github.com>
2025-02-16 09:33:14 +0530

    Time: 11 ms (65.60%) | Memory: 45.9 MB (23.69%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.java
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.java
@@ -1,28 +1,27 @@
-class Solution {
+import java.util.Arrays;
+
+public class Solution {
     public boolean closeStrings(String word1, String word2) {
-        if(word1.length() != word2.length()){
-           return false;
-        }
-        if(word1.equals(word2)){
-            return true;
-        }
-        Map<Character,Integer> freq1 = new HashMap<>();
-        Map<Character,Integer> freq2 = new HashMap<>();
+        if (word1.length() != word2.length()) return false;
 
-      
+        int[] freq1 = new int[26], freq2 = new int[26];
+        boolean[] exists1 = new boolean[26], exists2 = new boolean[26];
 
-        for(int i=0;i<word1.length();i++){
+        for (char c : word1.toCharArray()) {
+            freq1[c - 'a']++;
+            exists1[c - 'a'] = true;
+        }
 
-            freq1.put(word1.charAt(i),freq1.getOrDefault(word1.charAt(i),0)+1);
-            freq2.put(word2.charAt(i),freq2.getOrDefault(word2.charAt(i),0)+1);
+        for (char c : word2.toCharArray()) {
+            freq2[c - 'a']++;
+            exists2[c - 'a'] = true;
         }
-        if(!freq1.keySet().equals(freq2.keySet())){
-            return false;
-        }
-        List<Integer> values1 = new ArrayList<>(freq1.values());
-        List<Integer> values2 = new ArrayList<>(freq2.values());
-        Collections.sort(values1);
-        Collections.sort(values2);
-       return values1.equals(values2);
+
+        if (!Arrays.equals(exists1, exists2)) return false;
+
+        Arrays.sort(freq1);
+        Arrays.sort(freq2);
+
+        return Arrays.equals(freq1, freq2);
     }
 }
